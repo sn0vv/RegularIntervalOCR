1978a2838db07edd2dc6427e316290779321d755
sn0vv <sn0vv@users.noreply.github.com>
2021-03-15 22:57:39 +0900

    タイマー機能と、テキスト出力を実装した。

--- RegularIntervalOCR/MainWindow.cs
+++ RegularIntervalOCR/MainWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+//using System.Timers;
 
 namespace RegularIntervalOCR
 {
@@ -19,7 +20,10 @@ namespace RegularIntervalOCR
 
             var ocr = new OCR();
             var ocrResult = await ocr.RecognizeText(bmp);
-            richTextBox1.Text = ocrResult.Text;
+
+            var txt = ocrResult.Text;
+            richTextBox1.Text = txt;
+            ocr.SaveText(txt);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -31,5 +35,63 @@ namespace RegularIntervalOCR
         {
             UpdateOCR();
         }
+
+
+        public void Run()
+        {
+            Timer timer = new Timer();
+            timer.Tick += new EventHandler(MyClock);
+            timer.Interval = 3000;
+            timer.Enabled = true; // timer.Start()と同じ
+
+            //Application.Run(); // メッセージループを開始
+        }
+
+        public void MyClock(object sender, EventArgs e)
+        {
+            UpdateOCR();
+
+            //Console.WriteLine(DateTime.Now);
+            // 出力例：
+            // 2005/11/08 19:59:10
+            // 2005/11/08 19:59:11
+            // 2005/11/08 19:59:12
+            // ……
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Run();
+
+            //int num = 0;
+
+            //// タイマーの間隔(ミリ秒)
+            ////System.Timers.Timer timer = new System.Timers.Timer(3000);
+            //Timer timer = new Timer(3000);
+
+            //// タイマーの処理
+            ////timer.Elapsed += (sender, e) =>
+            //timer.Elapsed += (s, e2) =>
+            //{
+            //    if (num < 5)
+            //    {
+            //        UpdateOCR();
+            //        //Console.WriteLine("5回繰り返します");
+            //        num++;
+            //    }
+            //    else
+            //    {
+            //        timer.Stop();
+            //        //Console.WriteLine("処理を終了しました");
+            //    }
+            //};
+
+            //// タイマーを開始する
+            //timer.Start();
+
+            ////Console.ReadKey();
+        }
+
     }
 }
--- RegularIntervalOCR/OCR.cs
+++ RegularIntervalOCR/OCR.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using Windows.Graphics.Imaging;
 using Windows.Media.Ocr;
@@ -13,16 +14,42 @@ namespace RegularIntervalOCR
 
     public class OCR
     {
+        private static string folder;
+
+        public OCR()
+        {
+            folder = Directory.GetCurrentDirectory();
+        }
+
+        public void SaveText(string txt)
+        {
+            var fileName = "ocr_result.txt";
+            //StreamWriter sw = new StreamWriter(fileName, Encoding.GetEncoding("utf-8"));
+            //sw.Write(txt);
+            //sw.Close();
+
+            // 文字コードを指定
+            Encoding enc = Encoding.GetEncoding("utf-8");
+
+            // ファイルを開く
+            StreamWriter writer = new StreamWriter($@"{folder}\{fileName}", false, enc);
+
+            // テキストを書き込む
+            writer.WriteLine(txt);
+
+            // ファイルを閉じる
+            writer.Close();
+        }
 
         public async Task<SoftwareBitmap> ConvertToSoftwareBitmap(Bitmap bmp)
         {
             // 取得したキャプチャ画像をファイルとして保存
-            var folder = Directory.GetCurrentDirectory();
+            //var folder = Directory.GetCurrentDirectory();
+            //folder = Directory.GetCurrentDirectory();
             var fileName = "screenshot.bmp";
-            StorageFolder appFolder = await StorageFolder.GetFolderFromPathAsync(@folder);
-            //bmp.Save(folder + "\\" + fileName, ImageFormat.Bmp);
             bmp.Save($@"{folder}\{fileName}", ImageFormat.Bmp);
 
+            StorageFolder appFolder = await StorageFolder.GetFolderFromPathAsync(@folder);
             var bmpFile = await appFolder.GetFileAsync(fileName);
 
             // 保存した画像をSoftwareBitmap形式で読み込み
